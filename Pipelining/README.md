ba62278f1e21596e8cd73c6bb751ddbdc5db9b30
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-03-28 16:34:18 -0500

    Pipelining

---
+++ Pipelining/README.md
@@ -0,0 +1,6 @@
+# Pipelines in Go
+
+- create stages for specific tasks
+- has a start and end
+
+![alt text](image.png)
---
+++ Pipelining/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+
+func listToChannel(nums []int) <- chan int { // return a read only int channel
+	out := make(chan int) // unbuffered
+
+	go func() { // child 1 go routine
+		for _, n := range nums { // for each element in the list
+			out <- n // add to the channel
+		}
+		
+		close(out) // close the out channel created in this function
+	}()
+
+	return out // the function is async, so the function does not wait for the go routine to finish
+}
+
+func sq(in <- chan int) <- chan int { // return a read only int channel
+	out := make(chan int) // unbuffered
+
+	go func() { // child 1 go routine
+		for n := range in { // for each element in the in channel returned from listToChannel
+			out <- n * n // add the square to the channel
+		}
+		
+		close(out) // close the out channel created in this function
+	}()
+
+	return out // the function is async, so the function does not wait for the go routine to finish
+}
+
+// parent go routine
+func main() {
+	
+	// input
+	nums := []int{2, 3, 4, 5, 7, 2, 1}
+
+	// stage 1
+	dataChannel := listToChannel(nums) // pass the list of elements to a channel
+
+	// stage 2
+	finalChannel := sq(dataChannel) // pass the squared values of the elements to a channel
+
+	// stage 3
+	for i := range finalChannel {
+		fmt.Println(i) // print the squared values from the channel
+	}
+
+	// result: ... doing work for 3 seconds till the parent go routine cancels the child go routine
+}
