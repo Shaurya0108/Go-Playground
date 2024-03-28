d6f94431b4e6b9db494f166f753cb7e31a14cbd5
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-03-28 15:28:09 -0500

    comments

--- GoChannels/main.go
+++ GoChannels/main.go
@@ -6,7 +6,7 @@ func main() {
 	mainChannel := make(chan string) // channel opened
 
 	go func() {
-		mainChannel <- "Hello, World!"
+		mainChannel <- "Hello, World!" // channel recieves data: "Hello, World!"
 	}()
 
 	res := <- mainChannel // channel closed
