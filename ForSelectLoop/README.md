ba57aba3e709e3a396f6fe3318700f99845c8ae2
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-03-28 15:57:59 -0500

    async and sync

--- ForSelectLoop/README.md
+++ ForSelectLoop/README.md
@@ -1,1 +1,10 @@
 # For Select Loop Example
+
+![alt text](image.png)
+
+- A buffer acts as a queue where you can send the buffer length of data and it is treated as a queue
+  - does not worry about being blocked by the recieving go routine
+  - asynchronous
+- An unbuffered channel makes the go routine wait for a response from the reciever before it allows the sender to send data
+  - aka synchronous
+- 
--- ForSelectLoop/main.go
+++ ForSelectLoop/main.go
@@ -3,13 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	mainChannel := make(chan string) // channel opened
-
-	go func() { // go routine
-		mainChannel <- "Hello, World!" // channel recieves data: "Hello, World!"
-	}()
-
-	res := <- mainChannel // channel closed
-
-	fmt.Println(res)
+	// a buffer acts as a queue where you can send the buffer length of data and it is treated as a queue
+	bufferedChannel := make(chan string, 3) // opened a channel accepting data of type string with a buffer length of 3
 }
